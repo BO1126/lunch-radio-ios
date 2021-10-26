735c5fc22938ee31fe1c97bbe7ca0548766f5980
jungwoo Lee <b5nnnal88@gmail.com>
2021-10-27 08:15:10 +0900

    Set up Firebase Messaging

--- LunchRadio/LunchRadio/AppDelegate.swift
+++ LunchRadio/LunchRadio/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     override init() {
         super.init()
         UIFont.overrideInitialize()
+        sleep(1)
     }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
